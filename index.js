56514f01d953c2b07ea012b605b82725465df713
Christian Georgeou <cgeorgeou@hotmail.com>
2018-07-19 00:18:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function destructivelyRemoveFirstDriver() {
   drivers.shift();
 }
 function appendDriver(name) {
-  let updatedDrivers = (...drivers, "Broom"); 
+  let appendDriver = (...drivers, "Broom"); 
 }
